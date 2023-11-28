0c8474adcd10b9811c1cc2fe84bce9074ee8a125
majestyjnr <aidoojuniorsolomon@gmail.com>
2023-11-28 18:01:40 +0000

    Commit Updates

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const path = require('path')
 const app = new express()
 
+app.set("view engine", "ejs")
+
 // Serving Static content
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
 
 // Import routes
 const index = require("./routes/index")
